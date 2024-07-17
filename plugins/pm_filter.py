1ddfeb579a8639d660e85a714e1249143bcf3993
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-17 12:31:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -298,7 +298,7 @@ async def advantage_spoll_choker(bot, query):
                 ]]
                 if NO_RESULTS_MSG:
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                k == await reply_msg.edit_text(text=script.MVE_NT_FND, reply_markup=InlineKeyboardMarkup(button))
+                k = await query.message.edit(script.MVE_NT_FND, reply_markup=InlineKeyboardMarkup(button))
                 await asyncio.sleep(10)
                 await k.delete()
 
