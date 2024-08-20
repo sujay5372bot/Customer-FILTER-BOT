979b2f6e0b65119359311e629ad6c5e73545bf54
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-20 16:52:31 +0530

    indent error fixed

--- bot.py
+++ bot.py
@@ -63,19 +63,19 @@ async def start():
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
     await Media2.ensure_indexes()
-        #choose the right db by checking the free space
-        stats = await clientDB.command('dbStats')
-        #calculating the free db space from bytes to MB
-        free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
-        if SECONDDB_URI and free_dbSize<10: #if the primary db have less than 10MB left, use second DB.
-            tempDict["indexDB"] = SECONDDB_URI
-            logging.info(f"Since Primary DB have only {free_dbSize} MB left, Secondary DB will be used to store datas.")
-        elif SECONDDB_URI is None:
-            logging.error("Missing second DB URI !\n\nAdd SECONDDB_URI now !\n\nExiting...")
-            exit()
-        else:
-            logging.info(f"Since primary DB have enough space ({free_dbSize}MB) left, It will be used for storing datas.")
-        await choose_mediaDB()
+    #choose the right db by checking the free space
+    stats = await clientDB.command('dbStats')
+    #calculating the free db space from bytes to MB
+    free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
+    if SECONDDB_URI and free_dbSize<10: #if the primary db have less than 10MB left, use second DB.
+        tempDict["indexDB"] = SECONDDB_URI
+        logging.info(f"Since Primary DB have only {free_dbSize} MB left, Secondary DB will be used to store datas.")
+    elif SECONDDB_URI is None:
+        logging.error("Missing second DB URI !\n\nAdd SECONDDB_URI now !\n\nExiting...")
+        exit()
+    else:
+        logging.info(f"Since primary DB have enough space ({free_dbSize}MB) left, It will be used for storing datas.")
+    await choose_mediaDB()
     me = await TechVJBot.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
